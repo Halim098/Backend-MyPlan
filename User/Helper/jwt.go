b8b8f0c45d1ea1949004d65bcec107bc920ecfd7
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-15 09:19:16 +0700

    add helper jwt

---
+++ User/Helper/jwt.go
@@ -0,0 +1,27 @@
+package helper
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var key = []byte(os.Getenv("JWT_SECRET"))
+
+func GenerateToken(id int, username string) (string, error) {
+	tokenTTL, err := strconv.Atoi(os.Getenv("JWT_Time"))
+	if err != nil {
+		return "", err
+	}
+	
+	claims := jwt.MapClaims{
+		"username": username,
+		"id" : id,
+		"exp": time.Now().Add(time.Minute * time.Duration(tokenTTL)).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(key)
+}
