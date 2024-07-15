6337fcdce52592b58593ffc6540a23bcdc9a6f70
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-15 09:49:55 +0700

    add GetUser Route

--- User/Router/Router.go
+++ User/Router/Router.go
@@ -2,6 +2,7 @@ package Router
 
 import (
 	"MyPlant-User/Controller"
+	"MyPlant-User/Middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,5 +11,8 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.POST("/register", Controller.Register)
 	r.POST("/login", Controller.Login)
+
+	r.Use(Middleware.Auth())
+	r.GET("/user", Controller.GetUser)
 	return r
 }
