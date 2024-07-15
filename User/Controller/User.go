257e55e55c4e1c54134e97e98edc4885960c0bf3
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-15 09:03:51 +0700

    update controller (add user Login)

--- User/Controller/User.go
+++ User/Controller/User.go
@@ -33,3 +33,28 @@ func Register(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{"message": "User created"})
 }
+
+func Login(c *gin.Context) {
+	var user Model.User
+	var err error
+
+	err = c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	dbUser,err := Model.GetUserByUsername(user.Username, Database.DB)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	err = dbUser.ValidatePassword(user.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Wrong Password"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+}
