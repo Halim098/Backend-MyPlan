d612f2008fd5081d932f2c59f0d598a0aa54b91d
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-17 06:50:14 +0700

    try connection mongo

--- Note/main.go
+++ Note/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"MyPlan-Note/Database"
+)
+
+func main() {
+	client, ctx, cancel, err := Database.Connect("mongodb://localhost:27017")
+    if err != nil {
+        panic(err)
+    }
+
+    defer Database.Close(client, ctx, cancel)
+
+    Database.Ping(client, ctx)
+}
