7ed0d7b6631d14e17a881f1cc8245e929933904e
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-17 20:29:51 +0700

    add Model Note

---
+++ Note/Model/Note.go
@@ -0,0 +1,24 @@
+package Model
+
+import (
+	DB "MyPlan-Note/Database"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Content struct {
+	Title   string `json:"content_title" bson:"content_title"`
+	Content string `json:"content" bson:"content"`
+}
+
+type Note struct {
+	ID      primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Title   string             `json:"title" bson:"title"`
+	Content []Content          `json:"content" bson:"content"`
+}
+
+func GetCollection() *mongo.Collection {
+	return DB.Client.Database("myplan").Collection("notes")
+}
