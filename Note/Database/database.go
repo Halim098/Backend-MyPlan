a0d2c861b5392c26187c475a0fcc1f33e1a8f32e
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-17 20:25:24 +0700

    update database

--- Note/Database/database.go
+++ Note/Database/database.go
@@ -12,33 +12,42 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func Close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc){
+var Client *mongo.Client
+var Ctx context.Context
+
+func Close(client *mongo.Client, cancel context.CancelFunc){
 
     defer cancel()
 
     defer func(){
-        if err := client.Disconnect(ctx); err != nil{
+        if err := client.Disconnect(Ctx); err != nil{
             panic(err)
         }
     }()
 }
 
-func Connect()(*mongo.Client, context.Context, context.CancelFunc, error) {
+func Connect()(context.CancelFunc, error) {
+	var err error
+	var cancel context.CancelFunc
+
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 
 	uri := fmt.Sprintf("mongodb://%s:%s@%s:%s", user, password, host, port)
-    ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+    Ctx, cancel = context.WithTimeout(context.Background(), 30 * time.Second)
 
-    client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-    return client, ctx, cancel, err
+    Client, err = mongo.Connect(Ctx, options.Client().ApplyURI(uri))
+    return cancel, err
 }
 
-func Ping(client *mongo.Client, ctx context.Context) error {
+func Ping() error {
+	collection := Client.Database("myplan").Collection("notes")
 
-    if err := client.Ping(ctx, readpref.Primary()); err != nil {
+	fmt.Println(collection)
+
+    if err := Client.Ping(Ctx, readpref.Primary()); err != nil {
         return err
     }
     fmt.Println("connected successfully")
