0985cec8966fe4dbdfea8405a6216e9757d22a20
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-18 15:31:52 +0700

    add middleware auth

---
+++ Note/Middleware/Auth.go
@@ -0,0 +1,39 @@
+package Middleware
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var key = []byte(os.Getenv("JWT_SECRET"))
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenUser := c.Request.Header.Get("Authorization")
+	
+		tokenUser = strings.Replace(tokenUser, "Bearer ", "", 1)
+
+		token, err := jwt.Parse(tokenUser, func(token *jwt.Token) (interface{}, error) {
+			return key, nil
+		})
+
+
+		if err != nil{
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized 1"})
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if ok && token.Valid {
+			c.Set("username", claims["username"])
+			c.Next()
+		} else {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized 2"})
+		}
+	}
+}
+
