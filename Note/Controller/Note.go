398c6af4ac4b423c2d3c0ea1220767ddfade2bc1
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-07-18 16:54:14 +0700

    add update

--- Note/Controller/Note.go
+++ Note/Controller/Note.go
@@ -30,3 +30,32 @@ func InsertNoteHandler(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, note)
 }
+
+func UpdateNoteHandler(c *gin.Context) {
+	username, _ := c.Get("username")
+	id := c.Param("id")
+	
+	data, err := Model.FindOne(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "ID not found"})
+		return
+	}
+
+	if data.Username != username.(string) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	result, err := data.Update(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update note"})
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
